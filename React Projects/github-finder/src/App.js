68068e12afc103ea2d7b708b6b86639d3263d875
govardhan <govardhanaaleswara88@gmail.com>
2025-09-01 11:36:29 +0530

    project created

---
+++ React Projects/github-finder/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='bg-purple'>Heading </h1>
+     <button className='btn btn-primary'>button</button>
+    </div>
+  );
+}
+
+export default App;
